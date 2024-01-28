ae41ed6ca98475b6da229074439aacf7654f4002
dhruvashok <dhruvashok000@gmail.com>
2024-01-27 18:15:53 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function showOpportunity() {
+    document.getElementById("test").style.display = 'visible';
+}
+
+function changeColor(elem) {
+    elem.style.backgroundColor = "#CDB3D0";
+    showOpportunity();
+}
+
+function resetColor(elem) {
+    elem.style.backgroundColor = "white";
+}
